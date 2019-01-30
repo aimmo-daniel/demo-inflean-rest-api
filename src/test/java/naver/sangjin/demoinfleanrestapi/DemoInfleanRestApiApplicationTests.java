00896b3f5f26660471b5f555c44d91b98086e0c2
sangjin <sangjin0309@github.com>
2019-01-30 23:06:39 +0900

    Initial commit

---
+++ src/test/java/naver/sangjin/demoinfleanrestapi/DemoInfleanRestApiApplicationTests.java
@@ -0,0 +1,17 @@
+package naver.sangjin.demoinfleanrestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoInfleanRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

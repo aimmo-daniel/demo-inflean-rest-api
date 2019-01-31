973ec573d06b0aaab1b6080816097e9d8b857bc8
sangjin <sangjin0309@github.com>
2019-01-31 22:12:04 +0900

    테스트 구현2

--- src/test/java/naver/sangjin/demoinfleanrestapi/events/EventControllerTests.java
+++ src/test/java/naver/sangjin/demoinfleanrestapi/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package naver.sangjin.demoinfleanrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,15 +24,35 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     // 입력값들을 전달하면 JSON 응답으로 201이 나오는지 확인
-    // ** Location 헤더에 생성된 이벤트를 조회할 수 있는 URl 담겨 있는지 확인.
-    // ** id는 DB에 들어갈 때 자동생성된 값으로 나오는지 확인
+    // ** Location 헤더에 생성된 이벤트를 조회할 수 있는 URI 담겨 있는지 확인.
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 01, 31, 10,02))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019,02,01,8,30))
+                .beginEventDateTime(LocalDateTime.of(2019,02,02,10,20))
+                .endEventDateTime(LocalDateTime.of(2019,02,03,04,20))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("신갈동")
+                .build();
+
+
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaTypes.HAL_JSON))
-            .andExpect(status().isCreated());
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists());
     }
 
 }

57769658650368f4ea378138900976115a4be744
sangjin <sangjin0309@github.com>
2019-02-08 22:18:39 +0900

    Body for BadRequest

--- src/main/java/naver/sangjin/demoinfleanrestapi/events/EventController.java
+++ src/main/java/naver/sangjin/demoinfleanrestapi/events/EventController.java
@@ -41,12 +41,12 @@ public class EventController {
                 .description(eventDto.getDescription()) ...;*/
 
         if(errors.hasErrors()) {
-            return  ResponseEntity.badRequest().build();
+            return  ResponseEntity.badRequest().body(errors);
         }
 
         eventValidator.validate(eventDto, errors);
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);

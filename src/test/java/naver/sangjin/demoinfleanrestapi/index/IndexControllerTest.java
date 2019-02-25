868a8b4026fd8142f22ae76c657fb4d61ae2062e
sangjin <sangjin0309@github.com>
2019-02-25 23:05:41 +0900

    예외 테스트

--- src/test/java/naver/sangjin/demoinfleanrestapi/index/IndexControllerTest.java
+++ src/test/java/naver/sangjin/demoinfleanrestapi/index/IndexControllerTest.java
@@ -2,8 +2,6 @@ package naver.sangjin.demoinfleanrestapi.index;
 
 import naver.sangjin.demoinfleanrestapi.common.BaseControllerTest;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;

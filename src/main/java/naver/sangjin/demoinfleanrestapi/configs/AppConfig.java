6bf4d4183629d183bed611274523eddea53519a7
sangjin <sangjin0309@github.com>
2019-03-05 23:23:54 +0900

    스프링 시큐리티 폼 인증 설정방법

--- src/main/java/naver/sangjin/demoinfleanrestapi/configs/AppConfig.java
+++ src/main/java/naver/sangjin/demoinfleanrestapi/configs/AppConfig.java
@@ -1,10 +1,18 @@
 package naver.sangjin.demoinfleanrestapi.configs;
 
+import naver.sangjin.demoinfleanrestapi.accounts.Account;
+import naver.sangjin.demoinfleanrestapi.accounts.AccountService;
+import naver.sangjin.demoinfleanrestapi.accounts.AccounteRole;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -19,4 +27,22 @@ public class AppConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account sangjin = Account.builder()
+                        .email("sangjin@email.com")
+                        .password("sangjin")
+                        .roles(Set.of(AccounteRole.ADMIN, AccounteRole.USER))
+                        .build();
+                accountService.saveAccount(sangjin);
+            }
+        };
+    }
 }
--- src/test/java/naver/sangjin/demoinfleanrestapi/accounts/AccountServiceTest.java
+++ src/test/java/naver/sangjin/demoinfleanrestapi/accounts/AccountServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -29,26 +30,27 @@ public class AccountServiceTest {
     AccountService accountService;
 
     @Autowired
-    AccountRepository accountRepository;
+    PasswordEncoder passwordEncoder;
+
 
     @Test
     public void findByUsername() {
         // Given
-        String password = "zmazma24";
+        String password = "sangjin";
         String username = "sangjin@gmail.com";
         Account account = Account.builder()
                 .email(username)
                 .password(password)
                 .roles(Set.of(AccounteRole.ADMIN, AccounteRole.USER))
                 .build();
-    this.accountRepository.save(account);
+        this.accountService.saveAccount(account);
 
         // When
         UserDetailsService userDetailsService = (UserDetailsService)accountService;
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
         // Then
-        assertThat(userDetails.getPassword()).isEqualTo(password);
+        assertThat(this.passwordEncoder.matches(password, userDetails.getPassword())).isTrue();
     }
 
     @Test
